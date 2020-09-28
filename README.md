6c2f39c53eb8828e9674b8236c732210562b664c
psakiev <psakiev@sandia.gov>
2020-09-28 12:11:48 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyConfigs
+
+A collection of configurations that set things up the way I like them.
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# symlink files
+ln -s ${HOME}/.tmux.conf tmux.conf
+ln -s ${HOME}/.my_bash my_bash
+
+# install vim stuff
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# append files
+
+# determine architecture
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ "$machine" == "Mac" ];
+    echo 'export source ${HOME}/.my_bash >> ${HOME}/.bash_profile'
+else
+    echo 'export source ${HOME}/.my_bash >> ${HOME}/.bashrc'
+fi
